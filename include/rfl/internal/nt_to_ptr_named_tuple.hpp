a30e2777eadc0b1f726d66f8c93d1161b3db08e4
Patrick Urbanke <patrick@getml.com>
2023-11-19 23:13:00 +0100

    Used tuplet::tuple instead of std::tuple inside the NamedTuple

--- include/rfl/internal/nt_to_ptr_named_tuple.hpp
+++ include/rfl/internal/nt_to_ptr_named_tuple.hpp
@@ -5,6 +5,7 @@
 
 #include "rfl/Field.hpp"
 #include "rfl/make_named_tuple.hpp"
+#include "tuplet/tuple.hpp"
 
 namespace rfl {
 namespace internal {
@@ -13,40 +14,39 @@ namespace internal {
 /// the struct from a named tuple.
 template <class NamedTupleType, class... AlreadyExtracted>
 auto nt_to_ptr_named_tuple(NamedTupleType& _nt, AlreadyExtracted... _a) {
-    using Fields = typename NamedTupleType::Fields;
+  using Fields = typename NamedTupleType::Fields;
 
-    constexpr auto i = sizeof...(AlreadyExtracted);
-    constexpr auto num_fields = std::tuple_size<Fields>{};
+  constexpr auto i = sizeof...(AlreadyExtracted);
+  constexpr auto num_fields = std::tuple_size<Fields>{};
 
-    if constexpr (i == num_fields) {
-        return make_named_tuple(_a...);
-    } else {
-        using FieldType = typename std::tuple_element<i, Fields>::type;
-        using T = std::decay_t<typename FieldType::Type>;
-        return nt_to_ptr_named_tuple(
-            _nt, _a...,
-            Field<FieldType::name_, T*>(&std::get<i>(_nt.values())));
-    }
+  if constexpr (i == num_fields) {
+    return make_named_tuple(_a...);
+  } else {
+    using FieldType = typename std::tuple_element<i, Fields>::type;
+    using T = std::decay_t<typename FieldType::Type>;
+    return nt_to_ptr_named_tuple(
+        _nt, _a..., Field<FieldType::name_, T*>(&tuplet::get<i>(_nt.values())));
+  }
 }
 
 /// Generates a named tuple that contains pointers to the original values in
 /// the struct from a named tuple.
 template <class NamedTupleType, class... AlreadyExtracted>
 auto nt_to_ptr_named_tuple(const NamedTupleType& _nt, AlreadyExtracted... _a) {
-    using Fields = typename NamedTupleType::Fields;
+  using Fields = typename NamedTupleType::Fields;
 
-    constexpr auto i = sizeof...(AlreadyExtracted);
-    constexpr auto num_fields = std::tuple_size<Fields>{};
+  constexpr auto i = sizeof...(AlreadyExtracted);
+  constexpr auto num_fields = std::tuple_size<Fields>{};
 
-    if constexpr (i == num_fields) {
-        return make_named_tuple(_a...);
-    } else {
-        using FieldType = typename std::tuple_element<i, Fields>::type;
-        using T = std::decay_t<typename FieldType::Type>;
-        return nt_to_ptr_named_tuple(
-            _nt, _a...,
-            Field<FieldType::name_, const T*>(&std::get<i>(_nt.values())));
-    }
+  if constexpr (i == num_fields) {
+    return make_named_tuple(_a...);
+  } else {
+    using FieldType = typename std::tuple_element<i, Fields>::type;
+    using T = std::decay_t<typename FieldType::Type>;
+    return nt_to_ptr_named_tuple(
+        _nt, _a...,
+        Field<FieldType::name_, const T*>(&tuplet::get<i>(_nt.values())));
+  }
 }
 
 }  // namespace internal
